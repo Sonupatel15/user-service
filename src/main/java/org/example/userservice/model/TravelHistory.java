b162137d5cc3488cd0ef1f9b1cb32a0353ca127d
Sonupatel A L <19358sonupatel@gmail.com>
2025-04-06 14:25:17 +0530

    Added the functionalities for the Transaction functionalities

--- src/main/java/org/example/userservice/model/TravelHistory.java
+++ src/main/java/org/example/userservice/model/TravelHistory.java
@@ -1,4 +1,64 @@
 package org.example.userservice.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.example.userservice.enums.TravelStatus;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "travel_history")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class TravelHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "travel_id", updatable = false, nullable = false)
+    private UUID travelId;
+
+    @Column(name = "transaction_id", nullable = false)
+    private UUID transactionId;
+
+    @Column(name = "user_id", nullable = false)
+    private UUID userId;
+
+    @Column(name = "from_station", nullable = false)
+    private int fromStation;
+
+    @Column(name = "to_station", nullable = false)
+    private int toStation;
+
+    @Column(nullable = false, precision = 5, scale = 2)
+    private BigDecimal distance;
+
+    @Column(name = "base_fare", nullable = false, precision = 10, scale = 2)
+    private BigDecimal baseFare;
+
+    @Column(name = "penalty", nullable = false, precision = 10, scale = 2)
+    private BigDecimal penalty;
+
+    @Column(name = "total_fare", nullable = false, precision = 10, scale = 2)
+    private BigDecimal totalFare;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TravelStatus status;
+
+    @PrePersist
+    public void prePersist() {
+        if (penalty == null) {
+            penalty = BigDecimal.ZERO;
+        }
+        if (totalFare == null && baseFare != null) {
+            totalFare = baseFare.add(penalty);
+        }
+        if (status == null) {
+            status = TravelStatus.NOT_STARTED;
+        }
+    }
 }
--- src/main/java/org/example/userservice/repository/TravelHistoryRepository.java
+++ src/main/java/org/example/userservice/repository/TravelHistoryRepository.java
@@ -1,4 +1,13 @@
 package org.example.userservice.repository;
 
-public class TravelHistoryRepository {
+import org.example.userservice.model.TravelHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface TravelHistoryRepository extends JpaRepository<TravelHistory, UUID> {
+    Optional<TravelHistory> findByTravelId(UUID travelId);
+    boolean existsByTravelId(UUID travelId);
+
 }
